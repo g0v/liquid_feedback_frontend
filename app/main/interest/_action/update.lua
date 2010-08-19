e5ed12666227e4923362ac287b42e33a6de928ec
bsw <devnull@localhost>
2010-08-19 15:36:39 +0200

    Fixed locking in add/remove interest action

--- app/main/interest/_action/update.lua
+++ app/main/interest/_action/update.lua
@@ -2,7 +2,7 @@ local issue_id = assert(param.get("issue_id", atom.integer), "no issue id given"
 
 local interest = Interest:by_pk(issue_id, app.session.member.id)
 
-local issue = Issue:by_id(issue_id)
+local issue = Issue:new_selector():add_where{ "id = ?", issue_id }:for_share():single_object_mode():exec()
 
 if issue.closed then
   slot.put_into("error", _"This issue is already closed.")
