11a7905c798d580ec864f47e0adc4e93a3b05d65
jbe <devnull@localhost>
2010-10-17 01:44:13 +0200

    Added comment on "supporter"."auto_support" also in update file

--- update/core-update.v1.2.8-v1.2.9.sql
+++ update/core-update.v1.2.8-v1.2.9.sql
@@ -7,4 +7,6 @@ CREATE OR REPLACE VIEW "liquid_feedback_version" AS
 ALTER TABLE "supporter" ADD COLUMN
   "auto_support" BOOLEAN NOT NULL DEFAULT FALSE;
 
+COMMENT ON COLUMN "supporter"."auto_support" IS 'Supporting member does not want to confirm new drafts of the initiative';
+
 COMMIT;
