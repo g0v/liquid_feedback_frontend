50850663cce22efa4ab31984bee184306326191d
jbe <devnull@localhost>
2012-02-25 15:09:27 +0100

    Re-added "session" table to the core

--- core.sql
+++ core.sql
@@ -302,6 +302,22 @@ COMMENT ON COLUMN "ignored_member"."member_id"       IS 'Member ignoring someone
 COMMENT ON COLUMN "ignored_member"."other_member_id" IS 'Member being ignored';
 
 
+CREATE TABLE "session" (
+        "ident"                 TEXT            PRIMARY KEY,
+        "additional_secret"     TEXT,
+        "expiry"                TIMESTAMPTZ     NOT NULL DEFAULT now() + '24 hours',
+        "member_id"             INT8            REFERENCES "member" ("id") ON DELETE SET NULL,
+        "lang"                  TEXT );
+CREATE INDEX "session_expiry_idx" ON "session" ("expiry");
+
+COMMENT ON TABLE "session" IS 'Sessions, i.e. for a web-frontend or API layer';
+
+COMMENT ON COLUMN "session"."ident"             IS 'Secret session identifier (i.e. random string)';
+COMMENT ON COLUMN "session"."additional_secret" IS 'Additional field to store a secret, which can be used against CSRF attacks';
+COMMENT ON COLUMN "session"."member_id"         IS 'Reference to member, who is logged in';
+COMMENT ON COLUMN "session"."lang"              IS 'Language code of the selected language';
+
+
 CREATE TABLE "policy" (
         "id"                    SERIAL4         PRIMARY KEY,
         "index"                 INT4            NOT NULL,
