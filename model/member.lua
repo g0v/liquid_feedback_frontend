59fde61f7df8ba328c1f4d1ed983cc79db516b82
bsw <devnull@localhost>
2012-03-01 00:07:46 +0100

    Search only for activated members

--- model/member.lua
+++ model/member.lua
@@ -325,7 +325,7 @@ function Member:get_search_selector(search_string)
   return self:new_selector()
     :add_field( {'"highlight"("member"."name", ?)', search_string }, "name_highlighted")
     :add_where{ '"member"."text_search_data" @@ "text_search_query"(?)', search_string }
-    :add_where("active")
+    :add_where("activated NOTNULL AND active")
 end
 
 function Member.object:send_invitation()
