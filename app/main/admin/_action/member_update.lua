9fa6bfd7c39a02201517e6a280fe22bcd46659d6
bsw <devnull@localhost>
2012-02-29 20:35:12 +0100

    In single unit mode automatically add members to default unit

--- app/main/admin/_action/member_update.lua
+++ app/main/admin/_action/member_update.lua
@@ -1,4 +1,6 @@
-local member = Member:by_id(param.get_id()) or Member:new()
+local id = param.get_id()
+
+local member = Member:by_id(id) or Member:new()
 
 param.update(member, "identification", "notify_email", "admin")
 
@@ -11,10 +13,17 @@ local err = member:try_save()
 if err then
   slot.put_into("error", (_("Error while updating member, database reported:<br /><br /> (#{errormessage})"):gsub("#{errormessage}", tostring(err.message))))
   return false
+end
+
+if not id and config.single_unit_id then
+  local privilege = Privilege:new()
+  privilege.member_id = member.id
+  privilege.unit_id = config.single_unit_id
+  privilege.voting_right = true
+end
+
+if id then
+  slot.put_into("notice", _"Member successfully updated")
 else
-  if id then
-    slot.put_into("notice", _"Member successfully updated")
-  else
-    slot.put_into("notice", _"Member successfully registered")
-  end
+  slot.put_into("notice", _"Member successfully registered")
 end
