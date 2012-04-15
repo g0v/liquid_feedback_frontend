751f7704a910575eb64cc35bf8255f1e26c53a5e
bsw <devnull@localhost>
2012-04-15 12:32:59 +0200

    Updated member api key settings to core 2.0

--- app/main/member/_action/update_api_key.lua
+++ app/main/member/_action/update_api_key.lua
@@ -1,54 +1,27 @@
 
-local setting_key = "liquidfeedback_frontend_api_key"
-local setting = Setting:by_pk(app.session.member.id, setting_key)
-
 local api_key
 
-if param.get("delete", atom.boolean) then
+if param.get_id() and param.get("delete", atom.boolean) then
 
-  if setting then
-    setting:destroy()
+  local member_application = MemberApplication:by_id(param.get_id())
+  
+  if member_application then
+    member_application:destroy()
   end
 
+  slot.put_into("notice", _"API key has been deleted")
 else
 
-  if not setting then
-    setting = Setting:new()
-    setting.member_id = app.session.member.id
-    setting.key = setting_key
-  end
-
-  api_key = multirand.string(
+  local member_application = MemberApplication:new()
+  member_application.member_id = app.session.member_id
+  member_application.key = multirand.string(
     20,
     '23456789BCDFGHJKLMNPQRSTVWXYZbcdfghjkmnpqrstvwxyz'
   )
+  member_application.name = 'member'
+  member_application.comment = ''
+  member_application.access_level = 'full'
 
-  setting.value = api_key
-
-  setting:save()
-end
-
-
-local setting_key = "liquidfeedback_frontend_api_key_history"
-
-setting = SettingMap:new()
-setting.member_id = app.session.member.id
-setting.key = setting_key
-setting.subkey = db:query("SELECT now()")[1].now
-setting.value = api_key or ""
-local dberr = setting:try_save()
-
-if dberr then
-  if dberr:is_kind_of("IntegrityConstraintViolation.UniqueViolation") then
-    slot.put_into("error", _"The API key has been changed too fast.")
-    return
-  else
-    dberr:escalate()
-  end
-end
-
-if not api_key then
-  slot.put_into("notice", _"API key has been deleted")
-else
-  slot.put_into("notice", _"API key has been updated")
+  member_application:save()
+  slot.put_into("notice", _"API key has been created")
 end
