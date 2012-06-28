a59a89d2e54599d668083f2773a1d4f6edee498c
bsw <devnull@localhost>
2012-06-28 23:19:41 +0200

    Optical enhancements

--- app/main/index/usage_terms.lua
+++ app/main/index/usage_terms.lua
@@ -1,13 +1,4 @@
-slot.select("actions", function()
-  ui.link{
-    module = "index",
-    view = "about",
-    content = function()
-      ui.image{ static = "icons/16/cancel.png" }
-      slot.put(_"Back")
-    end
-  }
-end)
+slot.put("<br />")
 
 ui.container{
   attr = { class = "wiki use_terms" },
