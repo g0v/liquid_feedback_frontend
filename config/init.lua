2aa3ed23d2c5e6a9fccba43503f16a5b4116addf
bsw <devnull@localhost>
2012-06-29 10:36:56 +0200

    Going to v2.0.0-rc3

--- config/init.lua
+++ config/init.lua
@@ -3,7 +3,7 @@
 -- (except when you really know what you are doing!)
 -- ========================================================================
 
-config.app_version = "2.0.0-rc2"
+config.app_version = "2.0.0-rc3"
 
 if config.enabled_languages == nil then
   config.enabled_languages = { 'en', 'de', 'eo', 'el', 'hu' }
