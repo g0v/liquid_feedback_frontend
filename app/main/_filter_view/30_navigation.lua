5c3875bc5b053258792109660e79298787d6d5c1
bsw <devnull@localhost>
2012-07-23 00:42:39 +0200

    Translate LiquidFeedback application name if translation available

--- app/main/_filter_view/30_navigation.lua
+++ app/main/_filter_view/30_navigation.lua
@@ -2,7 +2,7 @@ slot.select('navigation', function()
 
   ui.link{
     content = function()
-      ui.tag{ attr = { class = "logo_liquidfeedback" }, content = "LiquidFeedback" }
+      ui.tag{ attr = { class = "logo_liquidfeedback" }, content = _"LiquidFeedback" }
       slot.put(" &middot; ")
       ui.tag{ content = config.instance_name }
     end,
