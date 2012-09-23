4c6c47ebb88a95573a5781e7fb413e01b2fa7cc4
bsw <devnull@localhost>
2012-09-23 16:43:36 +0200

    Going to v2.1.0

--- config/init.lua
+++ config/init.lua
@@ -3,7 +3,7 @@
 -- (except when you really know what you are doing!)
 -- ========================================================================
 
-config.app_version = "2.0.5"
+config.app_version = "2.1.0"
 
 if not config.password_hash_algorithm then
   config.password_hash_algorithm = "crypt_sha512"
