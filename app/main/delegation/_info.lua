204ff5c5a8754408bd21d205507a012ed20e37c9
cmr <dev@cmr.cx>
2012-10-08 05:04:35 +0200

    Show delegation page of other members

--- app/main/delegation/_info.lua
+++ app/main/delegation/_info.lua
@@ -17,85 +17,88 @@ local unit_id  = unit  and unit.id  or nil
 local area_id  = area  and area.id  or nil
 local issue_id = issue and issue.id or nil
 
-
-local function print_delegation_info()
-  
-  local participant_occured = false
-
-  -- configure how many delegates should be displayed
-  local show_max_delegates = 15
- 
-  local delegation_chain = Member:new_selector()
-    :add_field("delegation_chain.*")
-    :join({ "delegation_chain(?,?,?,?,FALSE)", app.session.member.id, unit_id, area_id, issue_id }, "delegation_chain", "member.id = delegation_chain.member_id")
-    :add_order_by("index")
-    :limit(show_max_delegates + 2)
-    :exec()
-  
-  for i, record in ipairs(delegation_chain) do
-    
-    if i == show_max_delegates + 2 then
-      slot.put("...")
-      break
-    end
-    
-    local style
-    --local overridden = (not issue or issue.state ~= 'voting') and record.overridden
-
-    if i == 2 then      
-      local text = _"delegates to"
-      ui.image{
-        attr = { class = "delegation_arrow", alt = text, title = text },
-        static = "delegation_arrow_24_horizontal.png"
-      }
-    end
-      
-    -- highlight if participating 
-    local class = "micro_avatar"
-    if participant_occured and record.participation then
-      participant_occured = true
-      class = class .. " highlighted"
-    end
-      
-    -- get name of member
-    local member = Member:by_id(record.member_id)
-      
-    execute.view{
-      module = "member_image",
-      view = "_show",
-      params = {
-        member_id = record.member_id,
-        class = class,
-        popup_text = member.name,
-        image_type = "avatar",
-        show_dummy = true
-      }
-    }
-
-  end 
- 
-end
-
-
--- link
-if app.session.member_id == member.id then
-  ui.link{
-    module = "delegation", view = "show", params = {
-      unit_id = unit_id,
-      area_id = area_id,
-      issue_id = issue_id,
-      back_module = request.get_module(),
-      back_view = request.get_view(),
-      back_id = param.get_id_cgi()
-    },
-    attr = { class = "delegation_info" }, content = function()
-      print_delegation_info()
-    end
-  }
+-- title of the link to the delegation page
+if issue then
+  link_title = _"Issue delegation"
+elseif area then
+  link_title = _"Area delegation"
 else
-  ui.container{
-    attr = { class = "delegation_info" }, content = function()
-      print_delegation_info()
-    end
-  }
+  link_title = config.single_object_mode and _"Global delegation" or _"Unit delegation"
 end
+
+-- serialize get-parameters
+local params = ''
+for key, value in pairs(param.get_all_cgi()) do
+  params = params .. key .. "=" .. value .. "&"
+end
+
+ui.link{
+  module = "delegation", view = "show", params = {
+    unit_id = unit_id,
+    area_id = area_id,
+    issue_id = issue_id,
+    member_id = member.id,
+    back_module = request.get_module(),
+    back_view = request.get_view(),
+    back_id = param.get_id_cgi(),
+    back_params = params
+  },
+  attr = { class = "delegation_info", title = link_title },
+  content = function()
+
+    local participant_occured = false
+  
+    -- configure how many delegates should be displayed
+    local show_max_delegates = 15
+   
+    local delegation_chain = Member:new_selector()
+      :add_field("delegation_chain.*")
+      :join({ "delegation_chain(?,?,?,?,FALSE)", app.session.member.id, unit_id, area_id, issue_id }, "delegation_chain", "member.id = delegation_chain.member_id")
+      :add_order_by("index")
+      :limit(show_max_delegates + 2)
+      :exec()
+    
+    for i, record in ipairs(delegation_chain) do
+      
+      if i == show_max_delegates + 2 then
+        slot.put("...")
+        break
+      end
+      
+      local style
+      --local overridden = (not issue or issue.state ~= 'voting') and record.overridden
+  
+      if i == 2 then      
+        local text = _"delegates to"
+        ui.image{
+          attr = { class = "delegation_arrow", alt = text, title = text },
+          static = "delegation_arrow_24_horizontal.png"
+        }
+      end
+        
+      -- highlight if participating 
+      local class = "micro_avatar"
+      if participant_occured and record.participation then
+        participant_occured = true
+        class = class .. " highlighted"
+      end
+        
+      -- get name of member
+      local member = Member:by_id(record.member_id)
+        
+      execute.view{
+        module = "member_image",
+        view = "_show",
+        params = {
+          member_id = record.member_id,
+          class = class,
+          popup_text = member.name,
+          image_type = "avatar",
+          show_dummy = true
+        }
+      }
+   
+    end
+
+  end
+}
--- app/main/delegation/show.lua
+++ app/main/delegation/show.lua
@@ -5,39 +5,55 @@ local voting_right_unit_id
 local current_trustee_id
 local current_trustee_name
 
+
+local member_id = param.get("member_id", atom.integer)
+if member_id == '' or member_id == app.session.member.id then
+  -- show own delegation
+  member = app.session.member
+  ui_title = ""
+else
+  -- show other member's delegation
+  member = Member:by_id(member_id)
+  if not member or not member.activated then
+    error("access denied")
+  end
+  ui_title = _("Member '#{member}'", { member =  member.name }) .. ": "
+end
+
+
 local unit = Unit:by_id(param.get("unit_id", atom.integer))
 if unit then
-  unit:load_delegation_info_once_for_member_id(app.session.member_id)
+  unit:load_delegation_info_once_for_member_id(member.id)
   voting_right_unit_id = unit.id
   if unit.delegation_info.own_delegation_scope == 'unit' then
     current_trustee_id = unit.delegation_info.first_trustee_id
     current_trustee_name = unit.delegation_info.first_trustee_name
   end
-  ui.title(config.single_unit_id and _"Set global delegation" or _"Set unit delegation")
+  ui.title(ui_title .. (config.single_unit_id and _"Global delegation" or _"Unit delegation"))
   util.help("delegation.new.unit")
 end
 
 local area = Area:by_id(param.get("area_id", atom.integer))
 if area then
-  area:load_delegation_info_once_for_member_id(app.session.member_id)
+  area:load_delegation_info_once_for_member_id(member.id)
   voting_right_unit_id = area.unit_id
   if area.delegation_info.own_delegation_scope == 'area' then
     current_trustee_id = area.delegation_info.first_trustee_id
     current_trustee_name = area.delegation_info.first_trustee_name
   end
-  ui.title(_"Set delegation for Area '#{name}'":gsub("#{name}", area.name))
+  ui.title(ui_title .. (_"Delegation for Area '#{name}'":gsub("#{name}", area.name)))
   util.help("delegation.new.area")
 end
 
 local issue = Issue:by_id(param.get("issue_id", atom.integer))
 if issue then
-  issue:load("member_info", { member_id = app.session.member_id })
+  issue:load("member_info", { member_id = member.id })
   voting_right_unit_id = issue.area.unit_id
   if issue.member_info.own_delegation_scope == 'issue' then
     current_trustee_id = issue.member_info.first_trustee_id
     current_trustee_name = issue.member_info.first_trustee_name
   end
-  ui.title(_"Set delegation for Issue ##{number} in Area '#{area_name}'":gsub("#{number}", issue.id):gsub("#{area_name}", issue.area.name))
+  ui.title(ui_title .. (_"Delegation for Issue ##{number} in Area '#{area_name}'":gsub("#{number}", issue.id):gsub("#{area_name}", issue.area.name)))
   util.help("delegation.new.issue")
 end
 
@@ -75,10 +91,17 @@ end
 
 -- link back
 ui.actions(function()
+  -- unserialize get-parameters
+  local params = {}
+  local param_get = param.get("back_params") or ""
+  for key, value in string.gmatch(param_get, "([^=&]+)=([^=&]+)&") do
+    params[key] = value
+  end
   ui.link{
     module = param.get("back_module"),
     view = param.get("back_view"),
     id = param.get("back_id"),
+    params = params,
     content = _"Back"
   }
 end)
@@ -89,27 +112,27 @@ local issue
 
 if issue_id then
   issue = Issue:by_id(issue_id)
-  delegations = Delegation:by_pk(app.session.member.id, nil, nil, issue_id)
+  delegations = Delegation:by_pk(member.id, nil, nil, issue_id)
   if not delegations then
-    delegations = Delegation:by_pk(app.session.member.id, nil, issue.area_id)
+    delegations = Delegation:by_pk(member.id, nil, issue.area_id)
   end
   if not delegations then
-    delegations = Delegation:by_pk(app.session.member.id, issue.area.unit_id)
+    delegations = Delegation:by_pk(member.id, issue.area.unit_id)
   end
 elseif area_id then
-  delegations = Delegation:by_pk(app.session.member.id, nil, area_id)
+  delegations = Delegation:by_pk(member.id, nil, area_id)
   if not delegations then
     local area = Area:by_id(area_id)
-    delegations = Delegation:by_pk(app.session.member.id, area.unit_id)
+    delegations = Delegation:by_pk(member.id, area.unit_id)
   end
 end
 
 if not delegations then
-  delegations = Delegation:by_pk(app.session.member.id, unit_id)
+  delegations = Delegation:by_pk(member.id, unit_id)
 end
 
 local contact_members = Member:build_selector{
-  is_contact_of_member_id = app.session.member_id,
+  is_contact_of_member_id = member.id,
   voting_right_for_unit_id = voting_right_unit_id,
   active = true,
   order = "name"
@@ -132,13 +155,63 @@ for i, delegation in ipairs(delegations) do
     params = { member = Member:by_id(delegation.trustee_id) }
   }
       
-  ui.container{
-    attr = { class = "delegation_action" },
-    content = function()
-     
-      if i ~= 1 then
+  if member.id == app.session.member.id then
+    ui.container{
+      attr = { class = "delegation_action" },
+      content = function()
+       
+        if i ~= 1 then
+          ui.link{
+            text   = _"up",
+            module = "delegation",
+            action = "update",
+            params = {
+              unit_id = unit and unit.id or nil,
+              area_id = area and area.id or nil,
+              issue_id = issue and issue.id or nil,
+              initiative_id = initiative_id,
+              trustee_id = delegation.trustee_id,
+              trustee_swap_id = delegations[i-1].trustee_id
+            },
+            routing = {
+              default = {
+                mode = "redirect",
+                module = request.get_module(),
+                view = request.get_view(),
+                id = param.get_id_cgi(),
+                params = param.get_all_cgi()
+              }
+            }
+          }
+          slot.put("&nbsp;&nbsp;&nbsp;")
+        end
+        if i ~= #delegations then
+          ui.link{
+            text   = _"down",
+            module = "delegation",
+            action = "update",
+            params = {
+              unit_id = unit and unit.id or nil,
+              area_id = area and area.id or nil,
+              issue_id = issue and issue.id or nil,
+              initiative_id = initiative_id,
+              trustee_id = delegation.trustee_id,
+              trustee_swap_id = delegations[i+1].trustee_id
+            },
+            routing = {
+              default = {
+                mode = "redirect",
+                module = request.get_module(),
+                view = request.get_view(),
+                id = param.get_id_cgi(),
+                params = param.get_all_cgi()
+              }
+            }
+          }     
+          slot.put("&nbsp;&nbsp;&nbsp;")
+        end 
         ui.link{
-          text   = _"up",
+          text   = _"delete",
           module = "delegation",
           action = "update",
           params = {
@@ -147,7 +220,7 @@ for i, delegation in ipairs(delegations) do
             issue_id = issue and issue.id or nil,
             initiative_id = initiative_id,
             trustee_id = delegation.trustee_id,
-            trustee_swap_id = delegations[i-1].trustee_id
+            delete = true
           },
           routing = {
             default = {
@@ -159,137 +232,98 @@ for i, delegation in ipairs(delegations) do
             }
           }
         }
-        slot.put("&nbsp;&nbsp;&nbsp;")
+        
       end
-      if i ~= #delegations then
-        ui.link{
-          text   = _"down",
-          module = "delegation",
-          action = "update",
-          params = {
-            unit_id = unit and unit.id or nil,
-            area_id = area and area.id or nil,
-            issue_id = issue and issue.id or nil,
-            initiative_id = initiative_id,
-            trustee_id = delegation.trustee_id,
-            trustee_swap_id = delegations[i+1].trustee_id
-          },
-          routing = {
-            default = {
-              mode = "redirect",
-              module = request.get_module(),
-              view = request.get_view(),
-              id = param.get_id_cgi(),
-              params = param.get_all_cgi()
-            }
-          }
-        }     
-        slot.put("&nbsp;&nbsp;&nbsp;")
-      end 
-      ui.link{
-        text   = _"delete",
-        module = "delegation",
-        action = "update",
-        params = {
-          unit_id = unit and unit.id or nil,
-          area_id = area and area.id or nil,
-          issue_id = issue and issue.id or nil,
-          initiative_id = initiative_id,
-          trustee_id = delegation.trustee_id,
-          delete = true
-        },
-        routing = {
-          default = {
-            mode = "redirect",
-            module = request.get_module(),
-            view = request.get_view(),
-            id = param.get_id_cgi(),
-            params = param.get_all_cgi()
-          }
-        }
-      }
-      
-    end
-  }
-            
+    }
+  end
+                        
   slot.put("<br style='clear: left'/>")
 end    
+
+if #delegations == 0 then
+  ui.tag{
+    tag = "p",
+    attr = { style = "font-style:italic" },
+    content = _"There are no trustees selected."
+  }
+end
 
 
 -- add trustee
 
-ui.form{
-  attr = { class = "vertical", id = "delegationForm" },
-  module = "delegation",
-  action = "update",
-  params = {
-    unit_id = unit and unit.id or nil,
-    area_id = area and area.id or nil,
-    issue_id = issue and issue.id or nil,
-    initiative_id = initiative_id
-  },
-  routing = {
-    default = {
-      mode = "redirect",
-      module = request.get_module(),
-      view = request.get_view(),
-      id = param.get_id_cgi(),
-      params = param.get_all_cgi()
-    }
-  },
-  content = function()
-   
-    local records
-    records = {}
+if member.id == app.session.member.id then
+  ui.form{
+    attr = { class = "vertical", id = "delegationForm" },
+    module = "delegation",
+    action = "update",
+    params = {
+      unit_id = unit and unit.id or nil,
+      area_id = area and area.id or nil,
+      issue_id = issue and issue.id or nil,
+      initiative_id = initiative_id
+    },
+    routing = {
+      default = {
+        mode = "redirect",
+        module = request.get_module(),
+        view = request.get_view(),
+        id = param.get_id_cgi(),
+        params = param.get_all_cgi()
+      }
+    },
+    content = function()
+     
+      local records
+      records = {}
+    
+      -- add initiative authors
+      if initiative then
+        records[#records+1] = {id="_", name= "--- " .. _"Initiators" .. " ---"}
+        for i,record in ipairs(initiative.initiators) do
+          records[#records+1] = record.member
+        end
+      end
+      -- add saved members
+      if #contact_members > 0 then
+        records[#records+1] = {id="_", name= "--- " .. _"Saved contacts" .. " ---"}
+        for i, record in ipairs(contact_members) do
+          records[#records+1] = record
+        end
+      end
+      
+      disabled_records = {}
+      disabled_records["_"] = true
+      disabled_records[app.session.member.id] = true
+      -- disable members which are already in the list of trustees
+      for i, delegation in ipairs(delegations) do
+        disabled_records[delegation.trustee_id] = true
+      end
+      
+      ui.field.select{
+        name = "trustee_id",
+        foreign_records = records,
+        foreign_id = "id",
+        foreign_name = "name",
+        disabled_records = disabled_records
+      }
+      ui.submit{ text = _"Add to list" }
   
-    -- add initiative authors
-    if initiative then
-      records[#records+1] = {id="_", name= "--- " .. _"Initiators" .. " ---"}
-      for i,record in ipairs(initiative.initiators) do
-        records[#records+1] = record.member
-      end
+      ui.field.hidden{ name = "preview" }
+      
     end
-    -- add saved members
-    if #contact_members > 0 then
-      records[#records+1] = {id="_", name= "--- " .. _"Saved contacts" .. " ---"}
-      for i, record in ipairs(contact_members) do
-        records[#records+1] = record
-      end
-    end
-    
-    disabled_records = {}
-    disabled_records["_"] = true
-    disabled_records[app.session.member_id] = true
-    -- disable members which are already in the list of trustees
-    for i, delegation in ipairs(delegations) do
-      disabled_records[delegation.trustee_id] = true
-    end
-    
-    ui.field.select{
-      name = "trustee_id",
-      foreign_records = records,
-      foreign_id = "id",
-      foreign_name = "name",
-      disabled_records = disabled_records
-    }
-    ui.submit{ text = _"Add to list" }
-
-    ui.field.hidden{ name = "preview" }
-    
-  end
-}
+  }
+end
 
 
 slot.put("</td><td width='50%' valign='top'>")
 
 -- ------------------------
 
---slot.put("<hr>")
 ui.heading{ level = 2, content = _"Complete preference list over all scopes" }
 
 local delegation_chain = Member:new_selector()
   :add_field("delegation_chain.*")
-  :join({ "delegation_chain(?,?,?,?,TRUE)", app.session.member.id, unit_id, area_id, issue_id }, "delegation_chain", "member.id = delegation_chain.member_id")
+  :join({ "delegation_chain(?,?,?,?,TRUE)", member.id, unit_id, area_id, issue_id }, "delegation_chain", "member.id = delegation_chain.member_id")
   :add_order_by("index")
   :exec()
 
