329b5cdf1e6f4d742ffbc1e47e16496ad5cf58b4
cmr <dev@cmr.cx>
2012-11-01 22:11:11 +0100

    National flags

--- app/main/index/about.lua
+++ app/main/index/about.lua
@@ -23,5 +23,5 @@ slot.put("<br />")
 
 ui.field.text{ attr = { style = "font-weight: bold;" }, value = "3rd party license information:" }
 slot.put("<br />")
-slot.put('The icons used in Pirate Feedback (except national flags) are from <a href="http://www.famfamfam.com/lab/icons/silk/">Silk icon set 1.3</a> by Mark James. His work is licensed under a <a href="http://creativecommons.org/licenses/by/2.5/">Creative Commons Attribution 2.5 License.</a>')
+slot.put('The icons used in Pirate Feedback are from <a href="http://www.famfamfam.com/lab/icons/silk/">Silk icon set 1.3</a> by Mark James. His work is licensed under a <a href="http://creativecommons.org/licenses/by/2.5/">Creative Commons Attribution 2.5 License.</a>')
 
