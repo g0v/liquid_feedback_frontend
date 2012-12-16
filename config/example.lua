913e8e03a737de53350c80caa8a78d77e4d7dd12
cmr <dev@cmr.cx>
2012-12-16 13:11:55 +0100

    Rearrange example config

--- config/example.lua
+++ config/example.lua
@@ -89,6 +89,13 @@ config.public_access = "none"
 -- ------------------------------------------------------------------------
 -- config.delegation_warning_time = '6 months'
 
+-- Invite code expiry
+-- after how long is an invite code can't be used anymore
+-- notation is according to postgresql intervals
+-- Default: no expiry
+-- ------------------------------------------------------------------------
+-- config.invite_code_expiry = '1 month'
+
 -- Prefix of all automatic mails, defaults to "[Pirate Feedback] "
 -- ------------------------------------------------------------------------
 -- config.mail_subject_prefix = "[Pirate Feedback] "
@@ -99,20 +106,17 @@ config.public_access = "none"
 -- config.mail_from = { name = "Pirate Feedback", address = "pirate_feedback@example.com" }
 -- config.mail_reply_to = { name = "Support", address = "support@example.com" }
 
+-- Email for support
+-- is displayed if an invite code is expired or if there is no confirmed email address to send a passwort reset link to
+-- ------------------------------------------------------------------------
+-- config.support = "support@example.com"
+
 -- Supply custom url for avatar/photo delivery
 -- ------------------------------------------------------------------------
 -- Use the following option to enable fast image loading:
 -- config.fastpath_url_func = function(member_id, image_type)
 --   return request.get_absolute_baseurl() .. "fastpath/getpic?" .. tostring(member_id) .. "+" .. tostring(image_type)
 -- end
-
--- Local directory for database dumps offered for download
--- ------------------------------------------------------------------------
--- config.download_dir = nil
-
--- Special use terms for database dump download
--- ------------------------------------------------------------------------
--- config.download_use_terms = "=== Download use terms ===\n"
 
 -- Use custom image conversion, defaults to ImageMagick's convert
 -- ------------------------------------------------------------------------
@@ -122,19 +126,13 @@ config.public_access = "none"
 --  photo =  function(data) return extos.pfilter(data, "convert", "jpeg:-", "-thumbnail", "240x240", "jpeg:-") end
 --}
 
--- Display a message of the day once for each session
+-- WebMCP accelerator
+-- uncomment the following two lines to use C implementations of chosen
+-- functions and to disable garbage collection during the request, to
+-- increase speed:
 -- ------------------------------------------------------------------------
--- config.motd = "===Message of the day===\nThe MOTD is formatted with rocket wiki"
-
--- Display the message of the day not only to logged in members
--- ------------------------------------------------------------------------
--- config.motd_public = true
-
--- Automatic issue related discussion URL
--- ------------------------------------------------------------------------
--- config.issue_discussion_url_func = function(issue)
---   return "http://example.com/discussion/issue_" .. tostring(issue.id)
--- end
+-- require 'webmcp_accelerator'
+-- if cgi then collectgarbage("stop") end
 
 -- Integration of Etherpad, disabled by default
 -- ------------------------------------------------------------------------
@@ -146,13 +144,27 @@ config.public_access = "none"
 --  cookie_path = "/"
 --}
 
--- WebMCP accelerator
--- uncomment the following two lines to use C implementations of chosen
--- functions and to disable garbage collection during the request, to
--- increase speed:
+-- Automatic issue related discussion URL
 -- ------------------------------------------------------------------------
--- require 'webmcp_accelerator'
--- if cgi then collectgarbage("stop") end
+-- config.issue_discussion_url_func = function(issue)
+--   return "http://example.com/discussion/issue_" .. tostring(issue.id)
+-- end
+
+-- Local directory for database dumps offered for download
+-- ------------------------------------------------------------------------
+-- config.download_dir = nil
+
+-- Special use terms for database dump download
+-- ------------------------------------------------------------------------
+-- config.download_use_terms = "=== Download use terms ===\n"
+
+-- Display a message of the day once for each session
+-- ------------------------------------------------------------------------
+-- config.motd = "===Message of the day===\nThe MOTD is formatted with rocket wiki"
+
+-- Display the message of the day not only to logged in members
+-- ------------------------------------------------------------------------
+-- config.motd_public = true
 
 -- Trace debug
 -- uncomment the following line to enable debug trace
@@ -163,18 +175,6 @@ config.public_access = "none"
 -- uncomment the following line to allow registration without an invite code
 -- ------------------------------------------------------------------------
 -- config.register_without_invite_code = true
-
--- Invite code expiry
--- after how long is an invite code can't be used anymore
--- notation is according to postgresql intervals
--- Default: no expiry
--- ------------------------------------------------------------------------
--- config.invite_code_expiry = '1 month'
-
--- Email for support
--- is displayed if an invite code is expired or if there is no confirmed email address to send a passwort reset link to
--- ------------------------------------------------------------------------
--- config.support = "support@example.com"
 
 -- Member import:
 -- Maximum number of members which should be deactivated in one run
