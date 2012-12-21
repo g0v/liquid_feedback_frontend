12af7d11ef0a29ca210e5858af3853add2488870
cmr <dev@cmr.cx>
2012-12-21 17:43:42 +0100

    Title for suggestion

--- app/main/suggestion/show.lua
+++ app/main/suggestion/show.lua
@@ -21,42 +21,13 @@ end
 app.html_title.title = suggestion.name
 app.html_title.subtitle = _("Suggestion ##{id}", { id = suggestion.id })
 
-ui.title(function()
-  ui.link{
-    content = suggestion.initiative.issue.area.unit.name,
-    module = "unit",
-    view = "show",
-    id = suggestion.initiative.issue.area.unit.id
-  }
-  slot.put(" &middot; ")
-  ui.link{
-    content = suggestion.initiative.issue.area.name,
-    module = "area",
-    view = "show",
-    id = suggestion.initiative.issue.area.id
-  }
-  slot.put(" &middot; ")
-  ui.link{
-    content = _("Issue ##{id}", { id = suggestion.initiative.issue.id }),
-    module = "issue",
-    view = "show",
-    id = suggestion.initiative.issue.id
-  }
-  slot.put(" &middot; ")
-  slot.put(_"Suggestion for" .. " ")
-  ui.link{
-    content = _("Initiative i#{id}: #{name}", { id = suggestion.initiative.id, name = suggestion.initiative.name }),
-    module = "initiative",
-    view = "show",
-    id = suggestion.initiative.id
-  }
-end)
+ui.title(_"Suggestion", suggestion.initiative.issue.area.unit, suggestion.initiative.issue.area, suggestion.initiative.issue, suggestion.initiative)
 
 ui.actions(function()
   ui.link{
     content = function()
-        ui.image{ static = "icons/16/resultset_previous.png" }
-        slot.put(_"Back")
+      ui.image{ static = "icons/16/resultset_previous.png" }
+      slot.put(_"Back")
     end,
     module = "initiative",
     view = "show",
