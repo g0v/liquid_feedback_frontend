b28fc96c5201356e3e09b970ffa203bca437b8ed
cmr <dev@cmr.cx>
2013-04-17 01:54:09 +0200

    Additional direct issue quorum

--- app/main/admin/_action/policy_update.lua
+++ app/main/admin/_action/policy_update.lua
@@ -5,6 +5,7 @@ param.update(
   "index", "name", "description", "active",
   "admission_time", "discussion_time", "verification_time", "voting_time",
   "issue_quorum_num", "issue_quorum_den",
+  "issue_quorum_direct_num", "issue_quorum_direct_den",
   "initiative_quorum_num", "initiative_quorum_den",
   "direct_majority_num", "direct_majority_den", "direct_majority_strict", "direct_majority_positive", "direct_majority_non_negative",
   "indirect_majority_num", "indirect_majority_den", "indirect_majority_strict", "indirect_majority_positive", "indirect_majority_non_negative",
